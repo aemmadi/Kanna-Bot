bfce8593f2ce09d11b0704b02b005b523718dbd1
Anirudh Emmadi <kanna6501@gmail.com>
2018-05-26 23:54:32 -0500

    Added report command

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bot = new Discord.Client();
 //Ready message when bot successfully loads
 bot.on("ready", async() => {
     console.log(`${bot.user.username} is online!`);
-    bot.user.setActivity("on Kanna's laptop")
+    bot.user.setActivity("from Kanna's laptop", {type: "Watching"});
 })
 
 bot.on("message", async message => {
@@ -26,9 +26,17 @@ bot.on("message", async message => {
         return message.channel.send("Hello!");
     }
 
+    if(cmd == `${prefix}help`){
+        let embed = new Discord.RichEmbed()
+            .setTitle("## COMMANDS ##")
+            .setColor("#7ff441")
+            .addField("General", "!botinfo : Shows bot info\n!svinfo : Shows server info\n!report <username> : Allows user to report users\n!kick <username> : Allows admin to kick another user")
+            .addField("Music", "!play <youtube-url> : Plays the song from youtube servers");
+            return message.channel.send(embed);
+    }
     if(cmd == `${prefix}botinfo`){
         let embed = new Discord.RichEmbed()
-            .setTitle("## Bot Information ##")
+            .setTitle("## BOT INFORMATION ##")
             .setColor("#f4a442")
             .setThumbnail(bot.user.displayAvatarURL)
             .addField("Bot Name", bot.user.username)
@@ -38,7 +46,7 @@ bot.on("message", async message => {
 
     if(cmd == `${prefix}serverinfo` || cmd == `${prefix}svinfo`){
         let embed = new Discord.RichEmbed()
-            .setTitle("## Server Information ##")
+            .setTitle("## SERVER INFORMATION ##")
             .setColor("#418ff4")
             .setThumbnail(message.guild.iconURL)
             .addField("Server Name", message.guild.name)
@@ -46,9 +54,31 @@ bot.on("message", async message => {
             .addField("Created on", message.guild.createdAt)
             .addField("Server Region", message.guild.region)
             .addField("Server Owner", message.guild.owner)
-            .addField("Server Roles", message.guild.roles)
             .addField("Total Members", message.guild.memberCount);
             return message.channel.send(embed);
+    }
+
+    if(cmd == `${prefix}report`){
+        let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(!user)
+            return message.channel.send("Error. User not found, make sure you are using the right input: !report <usermention> [reason].");
+        let reason = args.join(" ").slice(22);
+        let embed = new Discord.RichEmbed()
+            .setTitle("## REPORTED DETAILS ##")
+            .setDescription("Report != kick/ban. Reporting a user gives admin a reason to keep an eye on the user reported")
+            .setColor("#f46441")
+            .addField("Reported User", `${user} with ID : ${user.id}`)
+            .addField("Reported By", `${message.author}`)
+            .addField("Channel", message.channel)
+            .addField("Reported Time", message.createdAt)
+            .addField("Reason", reason);
+
+            let channel = message.guild.channels.find('name', "reports");
+            if(!channel)
+                return message.channel.send("This server hasn't setup reports yet. To set it up, all you have to do is make a new text channel and give it the name 'reports'.")
+            message.delete().catch(O_o => {});
+            message.channel.send(`Report sent\nSuccessfully reported ${user}, Reason: ${reason}\nFull report available at '#reports' channel`);
+            return channel.send(embed);
     }
 })
 bot.login(botconfig.token);
