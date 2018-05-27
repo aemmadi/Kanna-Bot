7e1a12e05ff22d2b0d1af4cc9b25284d2a50754d
Anirudh Emmadi <kanna6501@gmail.com>
2018-05-26 22:43:14 -0500

    Added Commands added botinfo added serverinfo added comments

--- index.js
+++ index.js
@@ -15,4 +15,40 @@ bot.on("message", async message => {
     if(message.channel.type ==  'dm')
         return; //Don't respond to dm's sent to the bot
 
+    //Variable declarations
+    let prefix = botconfig.prefix;
+    let msgarray = message.content.split(' '); //Splits the msg everytime there is a space
+    let cmd = msgarray[0]; //Assigns the first word in msg to cmd variable. Ex: "!play"
+    let args = msgarray.slice(1); //Cuts off the cmd part of the msg and assigns the rest to args variable
+
+    //Commands
+    if(cmd == `${prefix}hello`){
+        return message.channel.send("Hello!");
+    }
+
+    if(cmd == `${prefix}botinfo`){
+        let boticon = bot.user.displayAvatarURL;
+        let embed = new Discord.RichEmbed()
+            .setTitle("## Bot Information ##")
+            .setColor("#f4a442")
+            .setThumbnail(boticon)
+            .addField("Bot Name", bot.user.username)
+            .addField("Created on", bot.user.createdAt);
+            return message.channel.send(embed);
+    }
+
+    if(cmd == `${prefix}serverinfo` || cmd == `${prefix}svinfo`){
+        let svicon = message.guild.iconURL;
+        let embed = new Discord.RichEmbed()
+            .setTitle("## Server Information ##")
+            .setColor("#418ff4")
+            .setThumbnail(svicon)
+            .addField("Server Name", message.guild.name)
+            .addField("Created on", message.guild.createdAt)
+            .addField("You joined on", message.guild.joinedAt)
+            .addField("Total Members", message.guild.memberCount);
+            return message.channel.send(embed);
+    }
+})
+
 bot.login(botconfig.token);
