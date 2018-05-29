1867992740db5f610aa122f38178e96fe0872996
Anirudh Emmadi <kanna6501@gmail.com>
2018-05-29 13:15:11 -0500

    Updated Help Command

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ bot.on("message", async message => {
         let embed = new Discord.RichEmbed()
             .setTitle("## COMMANDS ##")
             .setColor("#7ff441")
-            .addField("General", "`!botinfo` : Shows bot info\n`!svinfo` : Shows server info\n`!report <usermention>` : Allows user to report users\n`!kick <usermention> [reason]` : Allows admin to kick another user\n`!ban <usermention> [reason]` : Allows admin to ban users\n`!mute <usermention> [time s/m/h/d]` : Allows admin to mute an user for a certain period")
+            .addField("General", "`!botinfo` : Shows bot info\n`!svinfo` : Shows server info\n`!report <usermention>` : Allows user to report users\n`!kick <usermention> [reason]` : Allows admin to kick another user\n`!ban <usermention> [reason]` : Allows admin to ban users\n`!mute <usermention> [time s/m/h/d]` : Allows admin to mute an user for a certain period\n`!role [add/rmv] <usermention> {role-name}` : Allows admin to add or remove an user from a role")
             // .addField("Music", "!play <youtube-url> : Plays the song from youtube servers");
             return message.channel.send(embed);
     }
