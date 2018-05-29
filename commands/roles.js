9c948fdfb24af15b0aaaaae5bcd93f6110f803bb
Anirudh Emmadi <kanna6501@gmail.com>
2018-05-29 13:10:00 -0500

    Added Comments To Role Command

--- commands/roles.js
+++ commands/roles.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 
+//Start Command
 module.exports.run = async (bot, message, args) =>{
   if(!message.member.hasPermission("MANAGE_MEMBERS"))
     return message.channel.send("You don't have the permission to add/rmv roles on this server.");
@@ -12,6 +13,8 @@ module.exports.run = async (bot, message, args) =>{
   let rName = message.guild.roles.find(`name`, role);
   if(!rName)
     return message.channel.send("Couldn't find the role!");
+
+  //Checks if its addRole or removeRole
   if(args[0] == 'add'){
    if(rMember.roles.has(rName.id));
     await(rMember.addRole(rName.id));
@@ -22,9 +25,12 @@ module.exports.run = async (bot, message, args) =>{
     message.channel.send(`Congrats, <@${rMember.id}>, you have been given the role ${rName.name}. We  tried to DM, but ran into an error.`);
     }
   }
+
+  //Checks if its addRole or removeRole
   else if(args[0] == 'rmv'){
-    if(rMember.roles.has(rName.id));
-      await(rMember.removeRole(rName.id));
+    if(!rMember.roles.has(rName.id))
+      return message.channel.send(`Sorry, ${rMember} doesn't have the role ${rName}`);
+    await(rMember.removeRole(rName.id));
     
     try {
       await rMember.send(`You have been removed from the role '${rName.name}' on the server '${message.guild.name}'`);
@@ -32,11 +38,14 @@ module.exports.run = async (bot, message, args) =>{
       message.channel.send(`You have been removed from the role '${rName.name}' on the server '${message.guild.name}'. We tried to DM, but ran into an error`);
     }
   }
+
+  //Asks user to check the syntax
   else{
     return message.channel.send("Make sure you got the syntax right.\n!role [add/rmv] <usermention> {role-name}");
   }
 }
-
+//End Command
+//Prefix
 module.exports.help = {
   name: "role"
 }
