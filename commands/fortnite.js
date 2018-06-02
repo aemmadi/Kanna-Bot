3834e3b13f9d86194fbf47f605d0909e70f00a65
Anirudh Emmadi <kanna6501@gmail.com>
2018-06-02 18:07:31 -0500

    Added Fortnite Command

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -1,139 +1,50 @@
-// const Discord = require('discord.js');
+const Discord = require('discord.js');
 const config = require('../config.json');
-// const Fortnite = require('fortnite');
-// const ftnApi = new Fortnite(config.ftnApi);
+const Fortnite = require('fortnite');
+const ftnApi = new Fortnite(config.ftnApi);
 
-// module.exports.run = async (bot, message, args) =>{
-//   let username = args[0];
-//   let platform = args[1] || "pc";
+module.exports.run = async (bot, message, args) =>{
+  let username = args[0];
+  let platform = args[1] || "pc";
 
-//   let data = ftnApi.user(username, platform).then(data => {
-//     let stats = data.lifetimeStats;
-//     let kills = stats.find(s => s.stat == 'kills');
-//     let wins = stats.find(s => s.stat == 'wins');
-//     let kd = stats.find(s => s.stat == 'kd');
-//     let matches = stats.find(s => s.stat == 'matchesPlayed');
-//     let time = stats.find(s => s.stat == 'timePlayed');
-//     let avg = stats.find(s => s.stat == 'avgSurvivalTime');
+  if(!username)
+    return message.channel.send("Username not provided. Use the right syntax: !fbr <epic-username> [platform pc/xbl/psn]");
 
-//   let embed = new Discord.RichEmbed()
-//     .setTitle("## FORTNITE STATS ##")
-//     .setThumbnail('https://fortnitetracker.com/Images/General/logo.png')
-//     .setAuthor(data.username)
-//     .setColor('#f4b042')
-//     .addField("Kills", kills.value, true)
-//     .addField("Wins", wins.value, true)
-//     .addField("K/D", kd.value, true)
-//     .addField("Matches Played", matches.value, true)
-//     .addField("Time Played", time.value, true)
-//     .addField("Average Survival Time", avg.value, true);
+  let data = ftnApi.user(username, platform).then(data => {
+    let stats = data.stats;
+   // let solo = stats.solo;
+   //   let solo_score = solo.Mode["score"];
+   //   console.log(solo_score);
+  //  let seasonSolo = stats.current_solo;
+   // let duo = stats.duo;
+   // let seasonDuo = stats.current_duo;
+   // let squad = stats.squad;
+  //  let seasonSquad = stats.current_squad;
+    let lifetime = stats.lifetime;
+      let score = lifetime[6]['Score'];
+      let matches = lifetime[7]['Matches Played'];
+      let wins = lifetime[8]['Wins'];
+      let winPercent = lifetime[9]['Win%'];
+      let kills = lifetime[10]['Kills'];
+      let kd = lifetime[11]['K/d'];
 
-//     return message.channe.send(embed);
-//   }).catch(e => {
-//     console.log(e);
-//     message.channel.send("Error. User not found, make sure you are using the right input: !fbr <epic-username> [platform pc/xbl/psn].");
-//   })
-// }
-const request = require('request')
+    let embed = new Discord.RichEmbed()
+      .setTitle("## FORTNITE LIFETIME STATS ##")
+      .setThumbnail("https://fortnitetracker.com/Images/General/logo.png")
+      .setAuthor(data.username)
+      .setColor("#42b6f4")
+      .addField("Wins", wins, true)
+      .addField("Kills", kills, true)
+      .addField("K/D", kd, true)
+      .addField("Matches Played", matches, true)
+      .addField("Score", score, true)
+      .addField("Win Percentage", winPercent, true);
 
-module.exports = {
-  // Find Stats off of the args sent
-  stats: function(msg, args){
-    let platform = args[0]
-    if(platform) platform = platform.toLowerCase()
-    const username = args.slice(1,args.length).join('%20')
-
-    if(platform !== 'psn' && platform !== 'pc' && platform !== 'xbl' || platform === undefined){
-      msg.reply(`
-      The platform was not set correctly.
-      Please type in commands like so:
-      !stats pc BillyGates
-      !stats xbl MajorNelson
-      !stats psn KazHarai
-      `)
-      return null;
-    }
-    else if(username === undefined){
-      msg.reply(`
-      The username was not set.
-      Please type in commands like so:
-      !stats platform username
-      !stats pc BillyGates
-      `)
-      return null;
-    }
-
-    const options = {
-      'uri': `https://api.fortnitetracker.com/v1/profile/${platform}/${username}`,
-      'headers': {
-        'TRN-Api-Key': config.ftnApi,
-        'Content-Type': 'application/json'
-      }
-    }
-
-    request(options, (err, res, body) => {
-      if(err){
-        console.error(err)
-        msg.reply(`There was a problem getting stats for ${username.replace('%20', ' ')} on ${platform}.`)
-        return null
-      }
-      else if(res.statusCode === 200){
-        data = JSON.parse(body)
-        let soloWins = 0
-        let soloTime = '0h 0m'
-        let soloKd = 0
-        let duoWins = 0
-        let duoTime = '0h 0m'
-        let duoKd = 0
-        let squadWins = 0
-        let squadTime = '0h 0m'
-        let squadKd = 0
-
-        // Player Not Found
-        if(data.error){
-          msg.reply(`${data.error}`)
-          return null
-        }
-
-        // Solo
-        if(data.stats.p2){
-          soloWins = data.stats.p2.top1.displayValue
-          soloTime = data.stats.p2.minutesPlayed.displayValue
-          soloKd = data.stats.p2.kd.valueDec
-        }
-
-        // Duos
-        if(data.stats.p10){
-          duoWins = data.stats.p10.top1.displayValue
-          duoTime = data.stats.p10.minutesPlayed.displayValue
-          duoKd = data.stats.p10.kd.valueDec
-        }
-        
-        // Squad 
-        if(data.stats.p9){
-          squadWins = data.stats.p9.top1.displayValue
-          squadTime = data.stats.p9.minutesPlayed.displayValue
-          squadKd = data.stats.p9.kd.valueDec
-        }
-
-        // The most important line of code.
-        const user = username.replace(/b/ig, '🅱').replace('%20', ' ')
-
-        msg.reply(`
-        Stats for ${user} on ${platform}:
-        Gametype | # of Wins | Time Played | KD Ratio
-        Solo | ${soloWins} | ${soloTime} | ${soloKd}
-        Duo | ${duoWins} | ${duoTime} | ${duoKd}
-        Squad | ${squadWins} | ${squadTime} | ${squadKd}
-        Source: FORTNITE TRACKER - https://fortnitetracker.com/
-        `)
-      } 
-      else {
-        msg.reply(`Something went wrong. Sorry :b:ud.`)
-        return null
-      }
-    })
-  }
+      return message.channel.send(embed);
+  }).catch(e => {
+    console.log(e);
+    message.channel.send("Error. User not found, make sure you are using the right syntax: !fbr <epic-username> [platform pc/xbl/psn].");
+  })
 }
 
 module.exports.help = {
