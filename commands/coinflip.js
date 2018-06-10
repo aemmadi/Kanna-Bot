128ac471e385df437c904eada3d973d2b0cce367
Anirudh Emmadi <kanna6501@gmail.com>
2018-06-10 12:00:30 -0500

    Optimized coinflip command

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -4,16 +4,17 @@ module.exports.run = async (bot, message, args) =>{
   let heads = 1;
   let tails = 0;
   
-  let result = Math.floor(Math.random() * 3);;
+  let result = Math.floor(Math.random() * 2);;
 
   if(result == 0)
     return message.channel.send("TAILS");
   else if(result == 1)
     return message.channel.send("HEADS");
-  else
+  else{
     return message.channel.send("Error Occurred, Flip again.");
+  }
 }
 
 module.exports.help = {
-  name: "coinflip"
+  name: "flip"
 }
