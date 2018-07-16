ec1fc855adcb8a9dd62d175f992571c9608bbdf0
Anirudh Emmadi <kanna6501@gmail.com>
2018-07-15 19:45:38 -0500

    Added new locations for season 5

--- commands/fortnite-drop.js
+++ commands/fortnite-drop.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-  let places = ["Lazy Links", "Dusty Divot", "Fatal Fields", "Flush Factory", "Greasy Grove", "Haunted Hills", "Junk Junction", "Lonely Lodge", "Lucky Landing", "Paradise Palms", "Pleasant Park", "Retail Row", "Risky Reels", "Shifty Shafts", "Snobby Shores", "Tilted Towers", "Tomato Town", "Wailing Woods"];
+  let places = ["Lazy Links", "Dusty Divot", "Fatal Fields", "Flush Factory", "Greasy Grove", "Haunted Hills", "Junk Junction", "Lonely Lodge", "Lucky Landing", "Paradise Palms", "Pleasant Park", "Retail Row", "Risky Reels", "Shifty Shafts", "Snobby Shores", "Tilted Towers", "Tomato Town", "Wailing Woods", "Loot Lake", "Salty Springs"];
 
   let picker = Math.floor(Math.random() * places.length);
 
