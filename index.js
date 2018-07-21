39aa334f694af406017614edc17e67af4ee9ce85
Anirudh Emmadi <kanna6501@gmail.com>
2018-07-21 01:33:25 -0500

    Added roll command

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ bot.on("message", async message => {
             .setColor("#7ff441")
             .addField("General", "`!botinfo` : Shows bot info\n`!svinfo` : Shows server info\n`!report <usermention>` : Allows user to report users\n`!kick <usermention> [reason]` : Allows admin to kick another user\n`!ban <usermention> [reason]` : Allows admin to ban users\n`!mute <usermention> [time s/m/h/d]` : Allows admin to mute an user for a certain period\n`!role [add/rmv] <usermention> {role-name}` : Allows admin to add or remove an user from a role")
             .addField("Fortnite", "`!fbr <epic-username> [platform pc/xbl/psn] {mode all/season}` : Looks up stats for fortnite.\nFOR LIFETIME STATS `!fbr <epic-username> [platform pc/xbl/psn]`\n`!drop` : Randomly picks a spot on the fortnite map")
-            .addField("Misc.", "`!flip` : Flips a coin and replies HEADS or TAILS\n`!dog` : Random dog image or gif");
+            .addField("Misc.", "`!flip` : Flips a coin and replies HEADS or TAILS\n`!dog` : Random dog image or gif\n`!roll` : Rolls a dice");
             return message.channel.send(embed);
     }
 
