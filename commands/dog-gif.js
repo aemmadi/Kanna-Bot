7b1c54b767768d3070aae042b6fcb8cae1202e58
Anirudh Emmadi <kanna6501@gmail.com>
2018-07-31 11:37:19 -0400

    Fixed #11 Dog command error

--- commands/dog-gif.js
+++ commands/dog-gif.js
@@ -3,38 +3,42 @@ const superagent = require('superagent');
 
 //!dog
 module.exports.run = async (bot, message, args) => {
-  //Access API
-  let { body } = await superagent.get(`https://random.dog/woof.json`).on('error', err => {
+  let link = await getLink();
+
+  for(let i = 0; i < 10; i++){
+    if(i){
+      //Checks if the url is an image
+      if (link[link.length - 3] == "j" || link[link.length - 3] == "J" || link[link.length - 3] == "p") {
+        let embed = new Discord.RichEmbed()
+          .setColor("#f4b342")
+          .setTitle("## DOG IMAGE ##")
+          .setImage(link);
+
+        return message.channel.send(embed); //Sends image
+      }
+      //Checks for any GIFS 
+      else if (link[link.length - 3] == "g" || link[link.length - 3] == "G") {
+        let embed = new Discord.RichEmbed()
+          .setColor("#f4b342")
+          .setTitle("## DOG GIF ##")
+          .setImage(link);
+
+        return message.channel.send(embed); //Sends GIF
+      }
+      else {
+        link = await getLink();
+      }
+    }
+  }
+    return message.channel.send("Error Occurred. Try again later.\n\n **If this problem keeps arising, make sure you use the `!issue` command to report any issues with the bot**"); //Catches any unknown error
+  async function getLink() {
+    //Access API for random dog image/gif
+    let { body } = await superagent.get(`https://random.dog/woof.json`).on('error', err => {
       //console.log(err);
       return message.channel.send("Unknown Error Occurred.\n\n**If this problem keeps arising, make sure you use the `!issue` command to report any issues with the bot**");
-    }); //gets the JSON for random dog
-
-  const link = body.url; //url of dog
-
-  //Checks if the url is an image
-  if (link[link.length - 3] == "j" || link[link.length - 3] == "J" || link[link.length - 3] == "p") {
-    let embed = new Discord.RichEmbed()
-      .setColor("#f4b342")
-      .setTitle("## DOG IMAGE ##")
-      .setImage(link);
-
-    return message.channel.send(embed); //Sends image
-  }
-  //Checks if url is a video
-  else if (link[link.length - 3] == "m") {
-    return message.channel.send("Error Occurred. Try again."); //Sends error message, discord.js doesnt allow video embeds right now
-  }
-  //Checks for any GIFS 
-  else if (link[link.length - 3] == "g" || link[link.length - 3] == "G") {
-    let embed = new Discord.RichEmbed()
-      .setColor("#f4b342")
-      .setTitle("## DOG GIF ##")
-      .setImage(link);
-
-    return message.channel.send(embed); //Sends GIF
-  }
-  else {
-    return message.channel.send("Error Occurred. Try again later.\n\n **If this problem keeps arising, make sure you use the `!issue` command to report any issues with the bot**"); //Catches any unknown error
+    });
+    let link = body.url; //url of dog
+    return link;
   }
 }
 
