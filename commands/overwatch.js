c2f42765337350498099b148af19728b186b0ed1
Anirudh Emmadi <kanna6501@gmail.com>
2018-07-31 08:33:45 -0500

    Added comments

--- commands/overwatch.js
+++ commands/overwatch.js
@@ -3,17 +3,20 @@ const superagent = require('superagent');
 
 //!ow <username#battletag> [platform pc/psn/xbl]
 module.exports.run = async (bot, message, args) => {
+  //Checks if username is provided
   if(!args[0])
     return message.channel.send("Error. Please specify a username with a battleTag. `!ow <username#battleTag> [platform pc/psn/xbl]`");
 
   let username = args[0];
-  let result = checkUsername(username);
+  let result = checkUsername(username); //Converts username to API requirement. Ex: Kanna6501#1110 => Kanna6501-1110
 
+  //Checks if battleTag is provided
   if(!result)
     return message.channel.send("Error. Please provide your battle tag right after your username. `!ow <username#battleTag> [platform pc/psn/xbl]`");
   
   username = username.replace('#', '-');
   
+  //Checks if platform is provided
   if(!args[1])
     return message.channel.send("Error. Please specify a platform pc/psn/xbl. `!ow <username#battleTag> [platform pc/psn/xbl]`");
 
@@ -27,20 +30,24 @@ module.exports.run = async (bot, message, args) => {
     platform = "xbl";
   }
 
+  //Checks if provided platform matches with required platform format pc/psn/xbl
   if(platform == "wrong")
     return message.channel.send("Error. Please specify a **valid** platform pc/psn/xbl. `!ow <username#battleTag> [platform pc/psn/xbl]`");
   
+  //Accesses the API
   let { body } = await superagent.get(`http://ow-api.herokuapp.com/profile/${platform}/global/${username}`).on("error", err => {
     //console.error(err);
     return message.channel.send("Error occurred while retrieving player stats. Please try again later. `!ow <username#battleTag> [platform pc/psn/xbl]`\n\n **If this problem keeps arising, make sure you use the `!issue` command to report any issues with the bot**");
   });
 
+  //API error handling
   if (Object.keys(body).length === 0)
     return message.channel.send("Error occurred while retrieving player stats. Please try again later. `!ow <username#battleTag> [platform pc/psn/xbl]`\n\n **If this problem keeps arising, make sure you use the `!issue` command to report any issues with the bot**");
   
+  //Checks if profile is set to private
   if (body.private)
-    return message.reply("Blizzard is not letting me get your stats. Make your profile public to have access to your stats. You can do it by going to **Options > Social > Change profile to public** in the game client.");
-    
+    return message.reply("Blizzard is not letting me get the player stats. Player profile needs to be public to have access to stats. You can do it by going to **Options > Social > Change profile to public** in the game client.");
+
   let embed = new Discord.RichEmbed()
     .setTitle(`## OVERWATCH STATS FOR ${body.username.toUpperCase()} ##`)
     .setThumbnail(`${body.portrait}`)
@@ -50,7 +57,7 @@ module.exports.run = async (bot, message, args) => {
     .addField(`Quickplay`, `**Wins** : ${body.games.quickplay.won}`, true)
     .addField(`Competitive`, `**Rank** : ${body.competitive.rank}\n**Wins** : ${body.games.competitive.won}\n**Matches Played** : ${body.games.competitive.played}\n**Win Rate** : ${body.games.competitive.win_rate}%`, true);
   
-  return message.channel.send(embed);
+  return message.channel.send(embed); //Sends stats
 
   function checkUsername(username){
     let result = false;
