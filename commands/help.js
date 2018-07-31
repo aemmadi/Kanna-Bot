06728b769824c6172a7f8e5d89209c7cb5466712
Anirudh Emmadi <kanna6501@gmail.com>
2018-07-31 12:05:26 -0400

    Added new command syntax

--- commands/help.js
+++ commands/help.js
@@ -8,6 +8,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("General", "`!info` : Shows bot and server info\n`!report <usermention>` : Allows user to report users\n`!kick <usermention> [reason]` : Allows admin to kick another user\n`!ban <usermention> [reason]` : Allows admin to ban users\n`!mute <usermention> [time s/m/h/d]` : Allows admin to mute an user for a certain period\n`!role [add/rmv] <usermention> {role-name}` : Allows admin to add or remove an user from a role")
     .addField("Fortnite", "`!fbr <epic-username> [platform pc/xbl/psn]` : Shows lifetime stats.\n`!fbr <epic-username> [platform pc/xbl/psn] {mode all/season}` : Looks up stats for solo/duo/squad or season stats.\n`!drop` : Randomly picks a spot on the fortnite map")
     .addField("League of Legends", "`!lol <summoner-name>` : Shows a basic profile overview of user.")
+    .addField("Overwatch", "`!ow <username#battleTag> [platform pc/psn/xbl]` : Shows profile stats of user.")
     .addField("Rocket League", "`!rl [platform pc/psn/xbl] <id>`  **<id>** refers to your STEAMID64, PSN Username, XboxGamerTag respectively : Shows Rocket League Stats")
     .addField("Crypto", "`!crypto` : Shows global statistics of crpytocurrencies\n`!crypto top` : Shows Top 10 crypto stats")
     .addField("Feedback", "`!issue` : Report an issue with the bot. Any unresponsive commands.\n`!donate` : Can keep the bot running by donating for server costs :)")
