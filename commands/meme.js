398bd0c18293c043a24642e0f931370452f6ecb5
Anirudh Emmadi <kanna6501@gmail.com>
2018-08-01 21:38:57 -0400

    Basic meme command done

--- commands/meme.js
+++ commands/meme.js
@@ -1,41 +1,82 @@
-const Discord = require('discord.js');
-const superagent = require('superagent');
+const Discord = require("discord.js");
+const superagent = require("superagent");
 
+//!meme - generates random meme
+//!meme <number> - generates <number> of random memes
+//Array of meme subreddits
+//For loop into JSON get title and url
 module.exports.run = async (bot, message, args) => {
-  let { body } = await superagent
-    .get(`https://api.imgflip.com/get_memes`); //gets the JSON for random meme
+  let subReddit = await getSubReddit();
+  console.log(subReddit);
+  let meme = await getMeme(subReddit);
 
-    console.log(body["Object"]);
-  const link = body.url;
-  console.log(link);
+  let embed = new Discord.RichEmbed()
+    .setTitle("## RANDOM FRESH MEME ##")
+    .setDescription(meme.title)
+    .setImage(meme.img);
 
-  // //Checks if the url is an image or video
-  // if (link[link.length - 3] == "j" || link[link.length - 3] == "J" || link[link.length - 3] == "p") {
-  //   let embed = new Discord.RichEmbed()
-  //     .setColor("#f4b342")
-  //     .setTitle("## DOG IMAGE ##")
-  //     .setImage(link);
+  return message.channel.send(embed);
 
-  //   return message.channel.send(embed);
-  // }
-  // else if (link[link.length - 3] == "m") {
-  //   return message.channel.send("Error Occurred. Try again.");
-  // }
+  async function getSubReddit() {
+    let memeSubs = [
+      "blackpeopletwitter",
+      "MemeEconomy",
+      "metal_me_irl",
+      "bee_irl",
+      "coaxedintoasnafu",
+      "195",
+      "shittyadviceanimals",
+      "wholesomememes",
+      "dankmemes",
+      "memes"
+    ];
+    let randomSub = randomNumber(memeSubs.length);
+    let subReddit = memeSubs[randomSub];
+    return subReddit;
+  }
 
-  // //Checks for any GIFS 
-  // else if (link[link.length - 3] == "g" || link[link.length - 3] == "G") {
-    let embed = new Discord.RichEmbed()
-      .setColor("#f4b342")
-      .setTitle("## DOG GIF ##")
-      .setImage(link);
+  async function getMeme(subReddit) {
+    let { body } = await superagent
+      .get(`https://www.reddit.com/r/${subReddit}/hot.json?limit=100`)
+      .on("error", err => {
+        //console.error(err);
+        return message.channel.send(
+          "Error occurred while retrieving memes. Try again. `!meme`.\n\n **If this problem keeps arising, make sure you use the `!issue` command to report any issues with the bot**"
+        );
+      });
+    let randomMeme = randomNumber(100);
+    let link = body.data.children[randomMeme].data.url;
+    let checkUrl = linkChecker(link);
 
-    return message.channel.send(embed);
-  
-  // else {
-  //   return message.channel.send("Error Occurred. Try again later.");
-  // }
-}
+    if (!checkUrl) {
+      let repeat = await getMeme();
+    }
+    let memeTitle = body.data.children[randomMeme].data.title;
+    let memeImg = body.data.children[randomMeme].data.url;
+
+    return {
+      title: memeTitle,
+      img: memeImg
+    };
+  }
+
+  function linkChecker(link) {
+    if (
+      link[link.length - 3].toLowerCase() == "j" || //jpg
+      link[link.length - 3].toLowerCase() == "p" || //png
+      link[link.length - 3].toLowerCase() == "g" //gif
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  function randomNumber(num) {
+    let picker = Math.floor(Math.random() * num);
+    return picker;
+  }
+};
 
 module.exports.help = {
   name: "meme"
-}  
+};
