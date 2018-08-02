d0a0eaee66eec11d707a3ebe96f38d66afb19cff
Anirudh Emmadi <kanna6501@gmail.com>
2018-08-02 15:26:20 -0400

    Updated with patreon

--- commands/donate.js
+++ commands/donate.js
@@ -7,6 +7,7 @@ module.exports.run = async (bot, message, args) => {
     .setThumbnail(
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHOsXWZlIhn2oplPZ6f7Mwipi03UdSobACMn7y0Rop9dzEgk-Ipw"
     )
+    .addField("Patreon (Primary)", "https://www.patreon.com/KannaDev")
     .addField("PayPal", "https://www.paypal.me/kanna6501")
     .addField("Bitcoin (BTC)", "3PHYokeWs7Smc7bYEy2i4tvLn7hPFgSquF")
     .addField("Ethereum (ETH)", "0xa3A65FFC89F3440553eE012f97609f2DA3C839B3");
