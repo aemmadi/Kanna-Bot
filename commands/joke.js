755aa3c5b83ba8b4b7eca760401ad3a11a324e66
Anirudh Emmadi <kanna6501@gmail.com>
2018-08-12 03:12:08 -0400

    Added joke command

--- commands/joke.js
+++ commands/joke.js
@@ -1,38 +1,61 @@
 const Discord = require("discord.js");
 const superagent = require("superagent");
 
-//.joke - gets a random dad joke
-//.joke <number> - gets <number> dad jokes
+//.joke - Gets a random joke from reddit
 module.exports.run = async (bot, message, args) => {
-  if (args[0]) {
-    let num = parseInt(args[0]); //Converts into <number> format.
-    if (num == "NaN" || num > 10 || num < 2)
-      //Checks if <number> meets requirements. b/w 2-10
-      return message.channel.send(
-        "Error. Spam number needs to be a **valid** number (2-10).\n`.joke` for random dad joke\n`.joke <number>` for generating `<number>` dad jokes "
-      ); //Error message
+  let joke = await getJoke(); //Gets joke
 
-    //Generates <number> random dad jokes
-    while (num > 1) {
-      await dadJoke(num);
-      num--;
-    }
+  if (joke.nsfw == "nsfw_in_sfw") {
+    message.channel.send(
+      "**This joke cointains NSFW content, and this text channel doesn't allow NSFW content**"
+    );
+    message.channel.send(
+      "Searching for a new joke...\n------------------------------------------------------------------"
+    );
+    let jokeAgain = await getJoke();
   }
 
-  await dadJoke(1); //Generate 1 random dad joke
+  return message.channel.send(`**${joke.title}**\n\`\`\`${joke.content}\`\`\``); //Sends joke
 
-  async function dadJoke(num) {
+  async function getJoke() {
+    //API Access
     let { body } = await superagent
-      .get(`https://icanhazdadjoke.com/`)
-      .set("Accept", "application/json")
+      .get(`https://www.reddit.com/r/Jokes/hot.json?limit=100`)
       .on("error", err => {
         //console.error(err);
         return message.channel.send(
-          "Error while retrieving dad jokes. Try again `.joke`\n\n **If this problem keeps arising, make sure you use the `.issue` command to report any issues with the bot**"
+          "Error occurred while retrieving jokes. Try again. `.realjoke`.\n\n **If this problem keeps arising, make sure you use the `.issue` command to report any issues with the bot**"
         );
       });
-    let joke = body.joke;
-    return message.channel.send(`**${num}. ${joke}**`);
+
+    let number = randomNumber(100); //Generates a random number
+    let nsfw = nsfwChecker(body, number); //NSFW filter
+
+    let jokeNsfw = "sfw_in_sfw";
+    if (nsfw) {
+      if (!message.channel.nsfw) {
+        jokeNsfw = "nsfw_in_sfw";
+      }
+    }
+
+    let jokeTitle = body.data.children[number].data.title; //Title
+    let jokeContent = body.data.children[number].data.selftext; //Joke
+
+    return {
+      title: jokeTitle,
+      content: jokeContent,
+      nsfw: jokeNsfw
+    };
+  }
+
+  function nsfwChecker(body, number) {
+    let nsfw = body.data.children[number].data.over_18; //Gets NSFW status
+    return nsfw;
+  }
+
+  function randomNumber(num) {
+    let picker = Math.floor(Math.random() * num); //Generates a random number
+    return picker;
   }
 };
 
