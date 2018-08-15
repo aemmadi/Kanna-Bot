860f0ae04a15878bf2014ae0ed5f4097be7f7a85
Anirudh Emmadi <kanna6501@gmail.com>
2018-08-15 18:06:57 -0500

    Need to fix empty post.content

--- commands/reddit.js
+++ commands/reddit.js
@@ -11,6 +11,7 @@ module.exports.run = async (bot, message, args) => {
     );
 
   let subReddit = args[0].replace("r/", "");
+  console.log(subReddit);
   let subCheck = await subRedditChecker(subReddit);
 
   if (!subCheck)
@@ -28,6 +29,26 @@ module.exports.run = async (bot, message, args) => {
 
   let post = await getRedditPost(subReddit, type);
 
+  if (post.nsfw) {
+    if (!message.channel.nsfw)
+      return message.channel.send(
+        "**This post contains NSFW content, and this text channel doesn't allow NSFW content**"
+      );
+  }
+
+  if (post.type == "text") {
+    //prettier-ignore
+    return message.channel.send(`##################################################\n\n**${post.title}**\n\n##################################################\n\nSubreddit: **${post.url}**\nUpvotes: **${post.upvotes}**\nAuthor: [${post.author}] (https://www.reddit.com/u/${post.author}/)\n\n##################################################\n\n${post.content}\n\n##################################################`);
+  } else if (post.type == "image") {
+    //prettier-ignore
+    let embed = new Discord.RichEmbed()
+      .setTitle("## REDDIT IMAGE POST ##")
+      .setDescription(`**${post.title}** | Scraped from **${post.url}** | Upvotes: **${post.upvotes}** | Author: **[${post.author}](https://www.reddit.com/u/${post.author})**`)
+      .setThumbnail(post.thumbnail)
+      .setImage(post.url);
+
+    return message.channel.send(embed);
+  }
   async function getRedditPost(subReddit) {
     let { body } = await superagent
       .get(`https://www.reddit.com/r/${subReddit}/${type}.json?limit=100`)
@@ -37,6 +58,30 @@ module.exports.run = async (bot, message, args) => {
           "Error occurred while checking the subreddit. Try again. `.help reddit`.\n\n **If this problem keeps arising, make sure you use the `.issue` command to report any issues with the bot**"
         );
       });
+
+    let random = randomNumber(100);
+    let selectedPost = body.data.children[random];
+
+    let media = getMediaType(selectedPost);
+
+    let title = selectedPost.data.title;
+    let content = selectedPost.data.selftext;
+    let thumbnail = selectedPost.data.thumbnail;
+    let url = selectedPost.data.url;
+    let upvotes = selectedPost.data.ups;
+    let author = selectedPost.data.author;
+    let nsfw = selectedPost.data.over_18;
+
+    return {
+      type: media.type,
+      title: title,
+      content: content,
+      upvotes: upvotes,
+      author: author,
+      nsfw: nsfw,
+      url: url,
+      thumbnail: thumbnail
+    };
   }
 
   async function subRedditChecker(subReddit) {
@@ -50,21 +95,25 @@ module.exports.run = async (bot, message, args) => {
       });
 
     if (
-      body.data.dist == 0 ||
-      body.data.after == null ||
+      body.data.dist == 0 &&
+      body.data.after == null &&
       body.data.before == null
     )
       return false;
     return true;
   }
 
-  function linkChecker(link) {
-    return link.match(/\.(jpeg|jpg|gif|png)$/) != null; //TRUE or FALSE. if url ends with .jpeg, .jpg, .png, or .gif
+  function getMediaType(selectedPost) {
+    let link = linkChecker(selectedPost.data.url);
+    let mediaType = "image";
+    if (!link) mediaType = "text";
+    return {
+      type: mediaType
+    };
   }
 
-  function nsfwChecker(body, randomMeme) {
-    let nsfw = body.data.children[randomMeme].data.over_18;
-    return nsfw;
+  function linkChecker(link) {
+    return link.match(/\.(jpeg|jpg|gif|png)$/) != null; //TRUE or FALSE. if url ends with .jpeg, .jpg, .png, or .gif
   }
 
   function randomNumber(num) {
