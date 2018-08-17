79de9908b14f8220a283898eb30c84fd858791c3
Anirudh Emmadi <kanna6501@gmail.com>
2018-08-17 12:37:10 -0500

    Still not done

--- index.js
+++ index.js
@@ -42,15 +42,35 @@ bot.on("ready", async () => {
   status_change(status); //Random status displays
 });
 
-// FIGURE THIS OUT
-bot.on("guildCreate", guild => {
-  const defaultChannel = guild.channels.find(c =>
-    c
-      .type("text")
-      .permissionsFor(guild.me)
-      .has("SEND_MESSAGES")
-  );
-  defaultChannel.send(`Welcome!`);
+const getDefaultChannel = async guild => {
+  // get "original" default channel
+  if (guild.channels.has(guild.id)) return guild.channels.get(guild.id);
+
+  // Check for a "general" channel, which is often default chat
+  if (guild.channels.exists("name", "general"))
+    return guild.channels.find("name", "general");
+  // Now we get into the heavy stuff: first channel in order where the bot can speak
+  // hold on to your hats!
+  return guild.channels
+    .filter(
+      c =>
+        c.type === "text" &&
+        c.permissionsFor(guild.client.user).has("SEND_MESSAGES")
+    )
+    .sort(
+      (a, b) =>
+        a.position - b.position ||
+        Long.fromString(a.id)
+          .sub(Long.fromString(b.id))
+          .toNumber()
+    )
+    .first();
+};
+
+// This is called as, for instance:
+bot.on("guildMemberAdd", member => {
+  const channel = getDefaultChannel(member.guild);
+  channel.send(`Welcome ${member} to the server, wooh!`);
 });
 
 bot.on("message", async message => {
