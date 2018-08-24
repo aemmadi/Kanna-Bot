96b9da011a91cd83d544b70cd048427a8ea26721
Anirudh Emmadi <kanna6501@gmail.com>
2018-08-24 14:28:36 -0500

    Fixed multiple words bug

--- commands/define.js
+++ commands/define.js
@@ -4,16 +4,16 @@ const superagent = require("superagent");
 //.def [term] - Gets definition of term from urban dictionary
 module.exports.run = async (bot, message, args) => {
   if (!args[0])
-    return message.channel.send("Error. Specify a term! `.def [term]`");
+    return message.channel.send("Error. Specify a term! `.def [term]`\nNote: if you have spaces in the term use `_` instead of spaces. Example: `.def netflix_and_chill`.");
 
-  //FIXME: SPACES IN TERM, REPLACE _ to %20
-  let term = args[0].toLowerCase();
+  let term = convertTerm(args[0]);
 
   let define = await getDefine(term);
+
   if (define.length == '3') {
     let embed = new Discord.RichEmbed()
       .setTitle(`## DEFINITION OF ${term.toUpperCase()} FROM URBAN DICTIONARY ##`)
-      .setDescription("**Note**: If you like the bot and want it to be active, use the **`.donate`** command to support the bot's server costs.")
+      .setDescription(`**View all**: ${define.url}\n**Note**: If you like the bot and want it to be active, use the **\`.donate\`** command to support the bot's server costs.`)
       .setThumbnail(`https://www.digzoo.com/sites/default/files/2017-09/image1_31.jpeg`)
       .setColor("#42f498")
       .addField("Term", define.term[0])
@@ -36,7 +36,7 @@ module.exports.run = async (bot, message, args) => {
 
   async function getDefine(term) {
     let { body } = await superagent.get(`http://urbanscraper.herokuapp.com/search/${term}`).on("error", err => {
-      return message.channel.send("An unknown error occurred while getting the definition. Try again. If it still doesn't work, use the **.issue** command to report the issue with the bot.");
+      return message.channel.send("**An unknown error occurred while getting the definition. Try again. If it still doesn't work, use the `.issue` command to report the issue with the bot.**");
     })
 
     let url = `http://www.urbandictionary.com/define.php?term=${term}`;
@@ -45,15 +45,18 @@ module.exports.run = async (bot, message, args) => {
     let topExamples = [];
 
     if (body.length > 2) {
-      for (let i = 0; i < 3; i++) {
-        topTerms.push(body[i].term);
-        topDefs.push(body[i].definition);
-        topExamples.push(body[i].example);
+      for (let i = 0; i < body.length; i++) {
+        if (body[i].definition.length < 1024) {
+          topTerms.push(body[i].term);
+          topDefs.push(body[i].definition);
+          topExamples.push(body[i].example);
+        }
       }
       return {
         term: topTerms,
         definition: topDefs,
         example: topExamples,
+        url: url,
         length: "3"
       };
     }
@@ -66,9 +69,21 @@ module.exports.run = async (bot, message, args) => {
         term: topTerms,
         definition: topDefs,
         example: topExamples,
+        url: url,
         length: "1"
       };
     }
+  }
+
+  function convertTerm(term) {
+    let converted = "";
+    for (let i = 0; i < term.length; i++) {
+      if (term.charAt(i) == "_") {
+        term = term.replace("_", "%20");
+      }
+    }
+    converted = term.toLowerCase();
+    return converted;
   }
 };
 
